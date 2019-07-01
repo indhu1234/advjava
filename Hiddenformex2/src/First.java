0fa89defc5732c17c1c0b7d11ae11c55325cda57
MR USER <MR USER@DESKTOP-9UC000P>
2019-07-01 19:39:38 +0530

    hidden form another one

---
+++ Hiddenformex2/src/First.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class First
+ */
+@WebServlet(name = "First", urlPatterns = { "/First" })
+public class First extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public First() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+	        String user = request.getParameter("user");
+        
+        //creating a new hidden form field
+        out.println("<form action='Secondserv'>");
+        out.println("<input type='hidden' name='user' value='"+user+"'>");
+        out.println("<input type='submit' value='submit' >");
+        out.println("</form>");
+
+	}
+
+}
