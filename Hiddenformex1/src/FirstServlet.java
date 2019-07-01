40fbf45d42e6668b85ed6845c7d8dda887c055af
MR USER <MR USER@DESKTOP-9UC000P>
2019-07-01 19:38:17 +0530

    Hidden form

---
+++ Hiddenformex1/src/FirstServlet.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		 try { 
+	            response.setContentType("text/html"); 
+	            PrintWriter out = response.getWriter(); 
+	  
+                String username = request.getParameter("userName"); 
+	            out.print("Welcome " + username); 
+	            out.print("<form action='SecondServlet'>"); 
+	  
+	            out.print("<input type='hidden' name='username' value='" + username + "'>"); 
+	            out.print("<input type='submit' value='submit'>"); 
+	            out.print("</form>"); 
+	            out.close(); 
+	        } 
+	        catch (Exception e) { 
+	            System.out.println(e); 
+	        } 
+	    } 
+	}
+
+
