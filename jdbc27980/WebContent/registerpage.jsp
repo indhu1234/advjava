119d70dd2b3e49234f74d5563bccd5c8e506aeb9
MR USER <MR USER@DESKTOP-9UC000P>
2020-08-30 13:46:49 +0530

    jdbc

---
+++ jdbc27980/WebContent/registerpage.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<pre>
+<form action="reg" method="post">
+<input type="number" hidden>
+Enter Name : <input type="text" name="n1">
+Enter Address : <textarea rows="10" cols="20"></textarea>
+Enter Email : <input type="email" name="e1">
+<input type="submit" value="Register Me">
+
+</form>
+</pre>
+</body>
+</html>
---
+++ jdbc27980/src/jdbc27980/Register.java
@@ -0,0 +1,36 @@
+package jdbc27980;
+
+public class Register
+{
+  int id;
+  String Name;
+  String address;
+  String email;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	Name = name;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+  
+  
+  
+}
