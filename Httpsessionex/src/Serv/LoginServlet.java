fb822047cacd1e2fba2e8cbd42b84576534b898e
MR USER <MR USER@DESKTOP-9UC000P>
2020-08-30 13:45:26 +0530

    session

---
+++ Httpsessionex/src/Serv/LoginServlet.java
@@ -0,0 +1,72 @@
+package Serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+//@WebServlet(name = "LoginServlet", urlPatterns = { "/LoginServlet" })
+public class LoginServlet extends HttpServlet {
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 response.setContentType("text/html");  
+	        PrintWriter out=response.getWriter();  
+	  request.getRequestDispatcher("link.html").include(request, response);  
+	          
+	        String name=request.getParameter("name");  
+	        String password=request.getParameter("password");  
+	          
+	        
+	        if(password.equals("admin123")){  
+	        out.print("Welcome, "+name);  
+	        HttpSession session=request.getSession();  
+	        session.setAttribute("name",name);  
+	        }  
+	        else{  
+	            out.print("Sorry, username or password error!");  
+  request.getRequestDispatcher("login.html").include(request, response);  
+	        }  
+	        out.close();  
+		
+		
+		//doGet(request, response);
+	}
+
+}
---
+++ Httpsessionex/src/Serv/LogoutServlet.java
@@ -0,0 +1,44 @@
+package Serv;
+
+import java.io.IOException;  
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+public class LogoutServlet extends HttpServlet 
+{
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                                throws ServletException, IOException {  
+            response.setContentType("text/html");  
+            PrintWriter out=response.getWriter();  
+              
+            request.getRequestDispatcher("link.html").include(request, response);  
+              
+            HttpSession session=request.getSession();  
+            session.invalidate();  
+              
+            out.print("You are successfully logged out!");  
+              
+            out.close();  
+    }  
+}  
